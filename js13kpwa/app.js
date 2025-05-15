c194b84304b5078de4a5e604b09cbfe9aa3eab7f
Eric Shen <sfs101203@nd.com.cn>
2025-05-15 12:52:19 +0800

    Refactor app.js and index.html for consistency and readability

--- js13kpwa/app.js
+++ js13kpwa/app.js
@@ -10,30 +10,31 @@ const template = `<article>
   <li><span>More:</span> <a href='http://js13kgames.com/entries/SLUG'>js13kgames.com/entries/SLUG</a></li>
   </ul>
 </article>`;
-let content = '';
+let content = "";
 for (let i = 0; i < games.length; i++) {
-  let entry = template.replace(/POS/g, (i + 1))
+  let entry = template
+    .replace(/POS/g, i + 1)
     .replace(/SLUG/g, games[i].slug)
     .replace(/NAME/g, games[i].name)
     .replace(/AUTHOR/g, games[i].author)
     .replace(/TWITTER/g, games[i].twitter)
     .replace(/WEBSITE/g, games[i].website)
     .replace(/GITHUB/g, games[i].github);
-  entry = entry.replace('<a href=\'http:///\'></a>', '-');
+  entry = entry.replace("<a href='http:///'></a>", "-");
   content += entry;
 }
-document.getElementById('content').innerHTML = content;
+document.getElementById("content").innerHTML = content;
 
 // Registering Service Worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwa-examples/js13kpwa/sw.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/pwa-examples/js13kpwa/sw.js");
 }
 
 // Requesting permission for Notifications after clicking on the button
-const button = document.getElementById('notifications');
-button.addEventListener('click', () => {
+const button = document.getElementById("notifications");
+button.addEventListener("click", () => {
   Notification.requestPermission().then((result) => {
-    if (result === 'granted') {
+    if (result === "granted") {
       randomNotification();
     }
   });
@@ -54,14 +55,14 @@ function randomNotification() {
 }
 
 // Progressive loading images
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
+  image.setAttribute("src", image.getAttribute("data-src"));
   image.onload = () => {
-    image.removeAttribute('data-src');
+    image.removeAttribute("data-src");
   };
 };
-if ('IntersectionObserver' in window) {
+if ("IntersectionObserver" in window) {
   const observer = new IntersectionObserver((items) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
@@ -78,3 +79,4 @@ if ('IntersectionObserver' in window) {
     loadImages(img);
   });
 }
+console.log("new version");
